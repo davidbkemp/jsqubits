f993d860288f6253152b16e9f6f95c74c5712287
David Kemp <davidbkemp@gmail.com>
2012-11-25 19:49:58 +1100

    Re-arranging and updating files ready to publish to the npm registry

--- resources-0.0.1/js/jsqubitsRunner.js
+++ resources-0.0.1/js/jsqubitsRunner.js
@@ -1,22 +1,7 @@
-/**
- *    Copyright 2012 David Kemp
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
-
-/**
- * For documentation, and the latest version, see http://www.jsqubits.org/
- */
+//     jsqubits
+//     http://jsqubits.org
+//     (c) 2012 David Kemp
+//     jsqubits may be freely distributed under the MIT license.
 
 /*jshint evil:true, eqnull:true, eqeqeq:true, forin:true, immed:true, latedef:true, newcap:true, noarg:true, nonew:true, regexp:true, undef:true, unused:true, strict:true, trailing:true */
 /*global jQuery, jsqubits, prompt, alert  */
